996eb2df6eca65094c2d1f12737219c4858ee408
Bifabig <biftubig@gmail.com>
2023-06-05 16:49:33 +0300

    add event handling for form inputs, checkbox and delete button

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import InputTodo from '@/components/InputTodo';
+import TodosList from '@/components/TodosList';
+import { v4 as uuidv4 } from 'uuid';
+
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: uuidv4(),
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+
+  const delTodo = (id) => {
+    setTodos([...todos.filter((todo) => todo.id != id)]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div>
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo} />
+    </div>
+  );
+};
+export default TodosLogic;
